cffb636bd25c13b12ff17b81d7825dda88ac932a
vera <vera@10kn.com>
2022-09-16 01:21:39 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+$('#mon').hover(
+    function () {
+        $(`#mon-hover`).addClass('active fix-absolute-mon');
+    },
+    function () {
+        $(`#mon-hover`).removeClass('active fix-absolute-mon');
+    }
+);
+
+$('#tue').hover(
+    function () {
+        $(`#tue-hover`).addClass('active fix-absolute-tue');
+    },
+    function () {
+        $(`#tue-hover`).removeClass('active fix-absolute-tue');
+    }
+);
+
+$('#wed').hover(
+    function () {
+        $(`#wed-hover`).addClass('active fix-absolute-wed');
+    },
+    function () {
+        $(`#wed-hover`).removeClass('active fix-absolute-wed');
+    }
+);
+
+$('#thu').hover(
+    function () {
+        $(`#thu-hover`).addClass('active fix-absolute-thu');
+    },
+    function () {
+        $(`#thu-hover`).removeClass('active fix-absolute-thu');
+    }
+);
+
+$('#fri').hover(
+    function () {
+        $(`#fri-hover`).addClass('active fix-absolute-fri');
+    },
+    function () {
+        $(`#fri-hover`).removeClass('active fix-absolute-fri');
+    }
+);
+
+$('#sat').hover(
+    function () {
+        $(`#sat-hover`).addClass('active fix-absolute-sat');
+    },
+    function () {
+        $(`#sat-hover`).removeClass('active fix-absolute-sat');
+    }
+);
+$('#sun').hover(
+    function () {
+        $(`#sun-hover`).addClass('active fix-absolute-sun');
+    },
+    function () {
+        $(`#sun-hover`).removeClass('active fix-absolute-sun');
+    }
+);
